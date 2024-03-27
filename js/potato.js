1608c2a7f62785f2cf24e70cc1a0661b8171a34c
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-03-27 11:47:19 +0100

    potatos location solved

--- js/potato.js
+++ js/potato.js
@@ -15,13 +15,13 @@ class Potato{
 
         for(let i = 0 ; i < 15 ; i++){
 
-            const maxX = this.gameScreen.clientWidth - 50; // 50 is the width of the coin
-            const maxY = 550; // 50 is the height of the coin
+            const maxX = this.gameScreen.clientWidth - 50; 
+            const maxY = this.gameScreen.clientHeight -200; 
             this.left = Math.floor(Math.random() * maxX);
             if(this.left < 80){
                 this.left += 80
             }
-            this.top = Math.floor(Math.random() *  (maxY - 300) + 300);
+            this.top = Math.floor(Math.random() *  (maxY - 300) + 190);
             this.element = document.createElement('img')
 
             this.element.src = 'images/potato.png'
@@ -30,13 +30,7 @@ class Potato{
             this.element.style.top = `${this.top}px`
             this.element.style.left = `${this.left}px`
             this.element.style.position = 'absolute'
-
             this.gameScreen.appendChild(this.element);
-
-            // const ediblePotato =  {
-
-            //     potatoRect : this.element.getBoundingClientRect(),
-            // }
             this.ediblePotatos.push(this.element)
             console.log(`ediblePotato:  ${this.ediblePotatos}`)
         }
@@ -45,13 +39,13 @@ class Potato{
         for(let i = 0 ; i < 20 ; i++){
 
 
-            const maxX = this.gameScreen.clientWidth - 50; // 50 is the width of the coin
-            const maxY = 550; // 50 is the height of the coin
+            const maxX = this.gameScreen.clientWidth - 50; 
+            const maxY = this.gameScreen.clientHeight-200; 
             this.left = Math.floor(Math.random() * maxX);
             if(this.left < 80){
                 this.left += 80
             }
-            this.top = Math.floor(Math.random() *  (maxY - 300) + 300);
+            this.top = Math.floor(Math.random() *  (maxY - 300) + 190);
             this.element = document.createElement('img')
 
             this.element.src = 'images/poisonous-potato.png'
